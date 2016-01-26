c15e95577062b20a7866040ea4f41702015935b5
Daniela Balmus <db483@cornell.edu>
2016-01-26 15:19:41 +0000

    Revert "adding changes # 3"
    This reverts commit b21d2b3184dcd59949bed83035ddb7e0dd86ce15.

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,7 +3,7 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Testing messages #3");
+		System.out.println("Testing messages");
 		
 		String text = "test message";
 		String owner = "Marlon Brando";
