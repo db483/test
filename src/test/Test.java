2caddf1a4bc457c6ab3c5bf67e683289c5443825
Daniela Balmus <db483@cornell.edu>
2016-01-26 15:02:15 +0000

    adding branch commit #2

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,12 +3,14 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Some text");
+		System.out.println("Testing messages");
+		
 		String text = "test message";
 		String owner = "Marlon Brando";
 		
 		Message message = new Message(text, owner);
 		System.out.println(message.toString());
+		
 		FancyMessage fancyMessage = new FancyMessage(text, owner);
 		System.out.println(fancyMessage.toString());
 	}
